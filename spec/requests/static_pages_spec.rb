e6a06de0c035ebc610411cb13f08f579ef0ac25e
Nina <ninamarie315@gmail.com>
2013-08-29 19:17:12 -0400

    changed my tests to use rspec let

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,32 @@
 require 'spec_helper'
 
+$title = "Rap Game Title"
+
 describe "StaticPages" do
+    describe "title let method" do
+        let(:title) { expect(page).to have_title($title) }
+
+        it "should have correct title" do
+            visit '/static_pages/home'
+            title.should
+        end
+        it "should have correct title" do
+            visit '/static_pages/about'
+            title.should
+        end
+        it "should have correct title" do
+            visit '/static_pages/contact'
+            title.should
+        end
+        it "should have correct title" do
+            visit '/static_pages/help'
+            title.should
+        end
+    end 
     describe "Home page" do
         it "should have the content 'Sample App'" do
             visit '/static_pages/home'
             expect(page).to have_content('Sample App')
-        end
-
-        it "should have the right title" do
-            visit '/static_pages/home'
-            expect(page).to have_title("Rap Game Home Title")
         end
     end
     describe "Help page" do
@@ -18,21 +35,17 @@ describe "StaticPages" do
             visit '/static_pages/help'
             expect(page).to have_content('Help')
         end
-
-        it "should have the right title" do
-            visit '/static_pages/help'
-            expect(page).to have_title("Rap Game Help Title")
-        end
     end
     describe "About page" do
         it "should have the content 'About Us'" do
             visit '/static_pages/about'
             expect(page).to have_content('About Us')
         end
-
-        it "should have the right title" do
-            visit '/static_pages/about'
-            expect(page).to have_title("Rap Game About Title")
+    end
+    describe "Contact page" do
+        it "should have the content 'Contact'" do
+            visit '/static_pages/contact'
+            expect(page).to have_content('Contact')
         end
     end
 end

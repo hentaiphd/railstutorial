ddf6901a717657f4c3e65820888652cfdd98bc39
Nina <ninamarie315@gmail.com>
2013-09-02 15:20:04 -0400

    setup home page before bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ SampleApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'home#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

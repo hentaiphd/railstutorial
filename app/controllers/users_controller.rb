2be3d5199241b566c682425181f374904b506808
Nina <ninamarie315@gmail.com>
2013-09-17 20:20:27 -0400

    finished part of the authentication and authorization bit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :signed_in_user, only: [:edit, :update]
+    
     def show
         @user = User.find(params[:id])
     end
@@ -32,15 +34,21 @@ class UsersController < ApplicationController
         end
     end
 
-    private
-
-    def user_params
-        params.require(:user).permit(:name, :email, :password, 
-            :password_confirmation)
-    end
-
     def destroy
         sign_out
         redirect_to root_url
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 end

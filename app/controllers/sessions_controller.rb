ffcea28b1d899dc6f039bbdb9086effb2683606f
Nina <ninamarie315@gmail.com>
2013-09-23 16:19:03 -0400

    Added user microposts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
-
+  
   def new
   end
 
-def create
+  def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,77 +1,71 @@
 class UsersController < ApplicationController
-    before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
-    before_action :correct_user,   only: [:edit, :update]
-    before_action :admin_user,     only: :destroy
+  before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
-    def index
-        @users = User.paginate(page: params[:page])
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  def new
+      @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
-    
-    def show
-        @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+    else
+        render 'new'
     end
+  end
 
-    def destroy
-      User.find(params[:id]).destroy
-      flash[:success] = "User destroyed."
-      redirect_to users_url
-    end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
 
-    def new
-        @user = User.new
-    end
+  private
 
-    def edit
-      end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 
-      def update
-        if @user.update_attributes(user_params)
-          flash[:success] = "Profile updated"
-          redirect_to @user
-        else
-          render 'edit'
-        end
-      end
+  # Before filters
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            sign_in @user
-            flash[:success] = "Welcome to the Sample App!"
-            redirect_to @user
-        else
-            render 'new'
-        end
-    end
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
-    def destroy
-      User.find(params[:id]).destroy
-      flash[:success] = "User destroyed."
-      redirect_to users_url
-    end
-
-    private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-
-    # Before filters
-
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end

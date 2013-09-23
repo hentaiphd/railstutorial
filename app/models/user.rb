3029c148b11fefccd4676d08b32f1a52e3eaef89
Nina <ninamarie315@gmail.com>
2013-09-23 17:04:12 -0400

    added following and followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,22 @@
 class User < ActiveRecord::Base
-    has_many :microposts, dependent: :destroy
-    before_save { self.email = email.downcase }
-    before_create :create_remember_token
+  has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :microposts, dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
-    validates :name, presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-        uniqueness: { case_sensitive: false }
-    has_secure_password
-    validates :password, length: { minimum: 6 }
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+      uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
@@ -21,6 +29,18 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy!
   end
 
   private
